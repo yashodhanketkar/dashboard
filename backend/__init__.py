e64668e5ce2b0349a502e70dea250efd59247f6b
yashodhanketkar <kykyashodhan@gmail.com>
2023-01-17 11:53:35 +0530

    0.1.0
    1. Updated version to 0.1.0 from 0.0.2.
    2. Added react production build to backend server.
    3. Updated run.py file to run both dev and prod server.
    4. Added logger for waitress (prod) server.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,19 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 from .util.config import configure
-from .routes.home.route import home
+
 from .routes.variables.route import variables
 from .database.models import db
+import logging
+
+
+# provide logs for waitress application production mode
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder="./build/static", template_folder="./build")
 
     with app.app_context():
         configure()
 
     db.init_app(app)
 
-    app.register_blueprint(home)
+    # provid access to react prod application
+    @app.route("/", methods=["GET"])
+    @app.route("/<path:path>")
+    def root(path=None):
+        return render_template("index.html")
+
     app.register_blueprint(variables)
 
     return app
